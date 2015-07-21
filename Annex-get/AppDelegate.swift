eebca412ab1ebaf6c8cc328c0fca6302969f61f7
Carl Moden <carl.moden@gmail.com>
2015-07-21 22:16:46 +0200

    Initial Commit

---
+++ Annex-get/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Annex-get
+//
+//  Created by Carl Moden on 2015-07-21.
+//  Copyright © 2015 Carl Moden. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Annex-getTests/Annex_getTests.swift
@@ -0,0 +1,36 @@
+//
+//  Annex_getTests.swift
+//  Annex-getTests
+//
+//  Created by Carl Moden on 2015-07-21.
+//  Copyright © 2015 Carl Moden. All rights reserved.
+//
+
+import XCTest
+@testable import Annex_get
+
+class Annex_getTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
