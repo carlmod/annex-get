506af2ce2e15f5c99bbbccab49ca428ea804f761
Carl Moden <carl.moden@gmail.com>
2015-07-24 09:03:59 +0200

    Working but trivial NSBrowser.

--- Annex-get/AppDelegate.swift
+++ Annex-get/AppDelegate.swift
@@ -6,22 +6,35 @@
 //  Copyright © 2015 Carl Moden. All rights reserved.
 //
 
+import AppKit
 import Cocoa
 
+
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSBrowserDelegate {
 
     @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var annexBrowser: NSBrowser!
+
 
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        annexBrowser.delegate = self
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
 
+    func browser(sender: NSBrowser, willDisplayCell cell: AnyObject, atRow row: Int, column: Int) {
+        let browserCell = cell as! NSBrowserCell
+        browserCell.stringValue = "AAA"
+    }
+    
+    func browser(sender: NSBrowser, numberOfRowsInColumn column: Int) -> Int {
+        let numRows=column + 1
+        return numRows
+    }
 
 }
 
